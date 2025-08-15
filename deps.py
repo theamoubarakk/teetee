35d7c37e36146e5e094d514fcee136996489e8b5
theamoubarakk <trm11@mail.aub.edu>
2025-08-15 11:57:06 +0300

    Update deps.py

--- deps.py
+++ deps.py
@@ -2,8 +2,4 @@ import streamlit as st
 import re
 from datetime import datetime
 
-# If you *really* want json/os here too, you can import and export them:
-# import json, os
-# __all__ = ["st", "re", "datetime", "json", "os"]
-
 __all__ = ["st", "re", "datetime"]
