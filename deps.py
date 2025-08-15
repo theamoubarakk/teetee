a1dfde1b932555303539f39e925324a1fac68450
theamoubarakk <trm11@mail.aub.edu>
2025-08-15 11:39:45 +0300

    Create deps.py

---
+++ deps.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import re
+from datetime import datetime
+
+# If you *really* want json/os here too, you can import and export them:
+# import json, os
+# __all__ = ["st", "re", "datetime", "json", "os"]
+
+__all__ = ["st", "re", "datetime"]
